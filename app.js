9aec4655c1295464a8a1ed2ffbe79fc6ff984c21
mahmoudkero35-netizen <mahmoudkero35@gmail.com>
2026-03-30 17:49:57 +0300

    Change server port from 3000 to 10000

--- app.js
+++ app.js
@@ -29,5 +29,5 @@ app.post("/webhook", (req, res) => {
 });
 
 // Use Render PORT
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
